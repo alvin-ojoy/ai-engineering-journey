630f929c009820c0eedb8c409e7cb881836d5c43
AlvinO <79180814+Justlearnfor@users.noreply.github.com>
2026-03-11 00:05:55 -0500

    Experiment with different prompts

--- day02-functions-and-api/ai-summarizer.py
+++ day02-functions-and-api/ai-summarizer.py
@@ -2,15 +2,15 @@ from openai import OpenAI
 
 client = OpenAI()
 
-def summarizer_text(text):
+def summarize_text(text):
     response = client.responses.create(
         model="gpt-5.4",
-        input=f"Summarize this text in 3 bullet points:\n\n{text}" 
+        input=f"Explain this text like I'm 10 years old:\n\n{text}" 
     )
     return response.output_text
 
-user_text = input("Paste text to summarize: ")
-summary = summarizer_text(user_text)
+user_text = input("Paste text to explain like you're a 10 years old: ")
+summary = summarize_text(user_text)
 
 print("\nAI Summary:\n")
 print(summary)
