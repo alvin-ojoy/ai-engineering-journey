907ebda4fdc3a11e2b6523010bfb0208b712d62d
AlvinO <79180814+Justlearnfor@users.noreply.github.com>
2026-03-10 23:19:48 -0500

    day01-python-basics

---
+++ day01-python-basics/madlib_ai.py
@@ -0,0 +1,7 @@
+name = input("What is your name: ")
+place = input("Enter a place: ")
+object = input("Enter an object: ")
+
+story = f"{name} went to {place} and found a mysterious{object}"
+
+print(story)
---
+++ day01-python-basics/variables.py
@@ -0,0 +1,7 @@
+age = 36
+height = 5.4
+is_student = False
+
+print(age)
+print(height)
+print(is_student)
